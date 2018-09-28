1aa7c06cdfd1da319ce2e867b332473b57c44c51
Toren Wallengren <toren.wallengren@addepar.com>
2018-09-27 19:53:54 -0600

    styling

--- frontend/src/components/GolCanvas.js
+++ frontend/src/components/GolCanvas.js
@@ -255,6 +255,7 @@ class GolCanvas extends Component {
                     width={this.props.scl * this.props.numwide}
                     height={this.props.scl * this.props.numhigh}
                     onClick={this.canvasClickHandler}
+                    style={{ 'border-radius': '5%' }}
                 />
 
                 {/* Call update function - starts animation when simulation status is 'RUNNING' (this.props.status === 1) */}
